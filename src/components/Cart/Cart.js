9df1315e8efe0ab3280c339dab3d0223c2248ee2
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-03-15 22:31:16 +0600

    Add Cart to Shop

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Cart = (props) => {
+    // console.log(props);
+    const cart = props.cart;
+    const total = cart.reduce((total, product) => total + product.price, 0);
+    /*let total = 0;
+    for(let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price;
+    }*/
+
+    let shipping = 0;
+    if(total > 35) {
+        shipping = 0;
+    }
+    else if(total > 15) {
+        shipping = 5.99;
+    }
+    else if(total > 0) {
+        shipping = 12.99;
+    }
+
+    const tax = total / 10;
+    const grandTotal = total + shipping + tax;
+
+    const formatNumber = num => {
+        const decimal = num.toFixed(2);
+        return Number(decimal);
+    }
+
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items Ordered: {cart.length}</p>
+            <p>Product Price: ${formatNumber(total)}</p>
+            <p><small>Shipping Cost: ${shipping}</small></p>
+            <p><small>Tax: ${formatNumber(tax)}</small></p>
+            <p>Total Price: ${formatNumber(grandTotal)}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 function Shop() {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
     const handleAddProduct = (product) => {
-        console.log('Product Added!');
-        console.log(product);
+        // console.log('Product Added!', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+        // console.log(newCart);
     };
     
     return (
@@ -24,7 +29,7 @@ function Shop() {
                 }
             </div>
             <div className="cart-container">
-                <h3>Cart</h3>
+                <Cart cart={cart}/>
             </div>
             
         </div>
