beb5806c6ed69c72ca47d18d34238f8905ac5329
mdajgarhossain <mdajgarhossain1@gmail.com>
2020-03-20 07:18:21 +0600

    Setup Router & Router path

--- src/App.js
+++ src/App.js
@@ -2,12 +2,39 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header />
-      <Shop />
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/review">
+            <Review />
+          </Route>
+          <Route path="/inventory">
+            <Inventory />
+          </Route>
+          <Route exact path="/">
+            <Shop />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div>
+            <h2>Sorry, page not found</h2>
+            <h1>404 Error!!!</h1>
+        </div>
+    )
+}
+
+export default NotFound
